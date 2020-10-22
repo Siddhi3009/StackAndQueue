960929f1c0ff7531fe6aae217c159a05f43f5bb4
Administrator <siddhiseth3009@gmail.com>
2020-10-22 20:11:54 +0530

    [Siddhi]Add. Push and Display method added in StackLinkedList

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -7,6 +7,11 @@ namespace StackAndQueue
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Stack and Queue demo");
+            StackLinkedList StacklinkedList = new StackLinkedList();
+            StacklinkedList.Push(70);
+            StacklinkedList.Push(30);
+            StacklinkedList.Push(56);
+            StacklinkedList.Display();
         }
     }
 }
