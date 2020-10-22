df27cee0dac8198289e0ed77392747201a7ebd25
Administrator <siddhiseth3009@gmail.com>
2020-10-22 20:39:29 +0530

    [Siddhi]Add. Enqueue and display method added to LinkedListQueue

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -13,6 +13,12 @@ namespace StackAndQueue
             StacklinkedList.Push(56);
             StacklinkedList.Display();
             StacklinkedList.IsEmpty();
+            Console.WriteLine("__________________________________________________________________");
+            LinkedListQueue linkedListQueue = new LinkedListQueue();
+            linkedListQueue.Enqueue(56);
+            linkedListQueue.Enqueue(30);
+            linkedListQueue.Enqueue(70);
+            linkedListQueue.Display();
         }
     }
 }
